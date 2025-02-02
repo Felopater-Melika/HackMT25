ff84e761c9ddc92a50ee785ff18fa83058126ec3
Christopher Keiningham <130263834+chriskein1@users.noreply.github.com>
2025-02-01 21:28:20 -0600

    Created schedule query

--- pyapi/scheduler/backgroundScheduler.py
+++ pyapi/scheduler/backgroundScheduler.py
@@ -1,35 +1,73 @@
 from apscheduler.schedulers.background import BackgroundScheduler
 import time
-import sqlalchemy
 import sqlite3
+import requests
 
 #init sqlite db 
 conn = sqlite3.connect('DB_NAME')
 cursor = conn.cursor()
 
 
-def my_daily_task():
+# Pass to server as patient data
+def bot_request():
+    patient_info = conn.execute(f"""
+    WITH todays_calls AS (
+        SELECT 
+            id AS call_id,
+            patient_id,
+            call_datetime
+        FROM schedule
+        WHERE DATE(call_datetime) = DATE('now')
+    )
+    SELECT 
+        tc.call_id,
+        tc.patient_id,
+        tc.call_datetime,
+        GROUP_CONCAT(DISTINCT pr.id) AS prescription_ids,  -- Multiple prescriptions?
+        p.id AS patient_id,
+        p.first_name,
+        p.phone_number,
+        p.caregivers,
+        pres.name AS prescription_name,
+        med.status AS medication_status
+    FROM todays_calls AS tc
+    JOIN patients AS p ON tc.patient_id = p.id
+    JOIN prescriptions AS pr ON tc.patient_id = pr.patient_id
+    JOIN medications AS med ON tc.patient_id = med.patient_id
+    GROUP BY tc.call_id, p.id, pres.name, med.status;
+    """).fetchall()
+    
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "patient_info": patient_info
+    }
+    
     url = "http://localhost:8000/make_call"
-    cursor.execute("""
-    SELECT 
-    patient,
-    patient_id,
-    phone_number,
-    FROM patients;""")
-    response = requests.get(url, '''TEMPORARY''')
+    
+    response = requests.post(url, json=data, headers=headers)
+    return response.json()
+    
 
-def get_call_times_from_db():
-    cursor.execute("""
+def get_todays_schedule_info():
+    todays_info = cursor.execute("""
     SELECT 
-    schedule_time,
-    id,
-    FROM medication_schedules;""")
+        id,
+        patient_id
+        call_datetime
+    FROM schedule
+    WHERE DATE(call_datetime) = DATE('now') LIMIT 1;
+    """)
+    
+    return todays_info.fetchall()
 
 def main():
     scheduler = BackgroundScheduler()
-    hour, minute = get_call_times_from_db()
+    id, patient_id, call_datetime = get_todays_schedule_info()
+    
+    hour = call_datetime.hour
+    minute = call_datetime.minute
 
-    scheduler.add_job(my_daily_task, 'cron', hour=hour, minute=minute)
+    scheduler.add_job(bot_request, 'cron', hour=hour, minute=minute)
     scheduler.start()
 
 main()
